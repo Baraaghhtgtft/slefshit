b013a351b635b55951fa0c79f71921da487bb3c1
Baraaghhtgtft <baraoo_1@hotmail.com>
2019-02-04 02:03:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
-var adminprefix = '^'
-
-
-const developers = ["419317864005238785"]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'بلاي')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'مشاهده')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'سمع')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ستريم')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
+client.on('ready', () => {
+  client.user.setGame(`Bye Bye Im Sleapiiing`,'https://www.twitch.tv/By:A7MD');
+  console.log('BOT ONLINE');
 });
-
-
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+client.login('NDQ1MjcwMDQ5Mzg5ODA1NTc5.DwTrIQ.cTOoTWouYddwN-2T5kAKm8zoPY0');
